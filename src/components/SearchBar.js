e511a1dfe4022ec400cada71f7ea62f8a1fd5fa2
charlie <charliemarroq@gmail.com>
2019-08-11 16:41:03 -0600

    error in consoleLog(this.) fixed by arrow function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,17 +3,8 @@ import React from 'react';
 class SearchBar extends React.Component{
     state = {term: "hi there!"}
 
-    /**
-     * When enter button is pressed inside an input text, it runs the default action,
-     * which is to submit the form. To prevent that, we'll use event.preventDefault
-     */
-    onFormSubmit(event){
+    onFormSubmit = (event) => {
         event.preventDefault();
-        /**
-         * next console log will throw an error
-         * explanation of this error is given on the course
-         * it's fixed by using arrow function instead.
-         */
         console.log(this.state.term);
     }
 
