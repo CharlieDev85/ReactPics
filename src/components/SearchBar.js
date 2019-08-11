e33a2fd0769264a2910b9dcee36a442053e85af2
charlie <charliemarroq@gmail.com>
2019-08-11 10:39:42 -0600

    changed to controlled element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
-    //this methods could be call whatever we want.
-    //the only one that matters is the property inside the input element
-    onInputChange(event){
-        console.log(event.target.value);
-    }
+    state = {term: "hi there!"}
 
-
-
-    //alternate way to handle events
-    //using an arrow function inside jsx
     render(){
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={(e)=>console.log(e.target.value)} />
+                        <input 
+                        type="text" 
+                        value={this.state.term}
+                        onChange={(e) => this.setState({ term: e.target.value })} 
+                        />
                     </div>
                 </form>
             </div>
