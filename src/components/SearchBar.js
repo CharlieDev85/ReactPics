604a8350290cca5483713f2a6985bcbe0616714a
charlie <charliemarroq@gmail.com>
2019-08-11 16:35:16 -0600

    working on OnFormSubmit, but error in console log

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,30 @@ import React from 'react';
 class SearchBar extends React.Component{
     state = {term: "hi there!"}
 
+    /**
+     * When enter button is pressed inside an input text, it runs the default action,
+     * which is to submit the form. To prevent that, we'll use event.preventDefault
+     */
+    onFormSubmit(event){
+        event.preventDefault();
+        /**
+         * next console log will throw an error
+         * explanation of this error is given on the course
+         * it's fixed by using arrow function instead.
+         */
+        console.log(this.state.term);
+    }
+
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
                         <input 
                         type="text" 
                         value={this.state.term}
-                        onChange={(e) => this.setState({ term: e.target.value })} 
+                        onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })} 
                         />
                     </div>
                 </form>
