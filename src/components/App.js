ad79f08d112ccb1029f093da73da44b3eff6ece1
charlie <charliemarroq@gmail.com>
2019-08-11 20:53:58 -0600

    handling request with async wait

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,14 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        //console.log(term);
-        axios.get(
+    async onSearchSubmit(term) {
+        const response = await axios.get(
             'https://api.unsplash.com/search/photos', {
                 params: { query: term },
                 headers: { Authorization: 'Client-ID 131bf192d653ab58b1aaf06a68c1ca0e6b416079496740606c15131cfa40bb26' }
             }
-        ).then(
-            /**
-             * we cn use this .then method to be able to get some console.log
-            */
-            (response) => {
-                console.log(response.data.results);
-            }
         );
+        console.log(response.data.results);
     }
 
     render() {
