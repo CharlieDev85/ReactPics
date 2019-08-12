e6db31c3fe8f910c4f9101529af9e4569e7844c9
charlie <charliemarroq@gmail.com>
2019-08-11 21:11:27 -0600

    async wait working, made as arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,24 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+state = { images:  [] };
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get(
             'https://api.unsplash.com/search/photos', {
                 params: { query: term },
                 headers: { Authorization: 'Client-ID 131bf192d653ab58b1aaf06a68c1ca0e6b416079496740606c15131cfa40bb26' }
             }
         );
-        console.log(response.data.results);
+        //console.log(this);
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui conteiner" style={{ marginTop: '10px' }}>
                 <SearchBar runMeWhenUserSubmits={this.onSearchSubmit} />
+                found : {this.state.images.length}images
             </div>
         );
     }
